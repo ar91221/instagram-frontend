978118fab551603812fdff83cfa4aba607231605
Arun Kumar <arunraj91221@gmail.com>
2026-06-22 23:08:52 +0530

    update backend url

--- src/App.js
+++ src/App.js
@@ -17,7 +17,7 @@ function App() {
     setDebugInfo(null);
 
     try {
-      const response = await axios.post('http://localhost:5000/api/auth/login', {
+      const response = await axios.post('http://instagram-backend.onrender.com/api/auth/login', {
         username,
         password
       });
